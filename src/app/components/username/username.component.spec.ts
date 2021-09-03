b9cd65ba941bfcb94a80a221e0a2c0a9faeedbee
amira-saya <amirasayari9@gmail.com>
2021-09-03 11:08:53 +0100

    client side

---
+++ src/app/components/username/username.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { UsernameComponent } from './username.component';
+
+describe('UsernameComponent', () => {
+  let component: UsernameComponent;
+  let fixture: ComponentFixture<UsernameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsernameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsernameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/username/username.component.ts
@@ -0,0 +1,17 @@
+import { Component, Output ,EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.scss']
+})
+export class UsernameComponent  {
+@Output() userNameEvent = new EventEmitter<string>();
+userName =''
+  constructor() { }
+
+  setUserName(): void {
+    this.userNameEvent.emit(this.userName);
+    
+  }
+}
